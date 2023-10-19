562f7323483151c405701fde1b978e25bf2e563c
unknown <skinghenry@gmail.com>
2023-10-19 17:32:22 -0400

    added for loops

--- src/SuperAutoProfessionals/ConsoleApp/Program.cs
+++ src/SuperAutoProfessionals/ConsoleApp/Program.cs
@@ -8,328 +8,82 @@ internal class Program
 	static void Main()
 	{
         //Creating the professionals
-        Console.WriteLine("Enter left team professional at 1");
-        Console.Write("CodeName:");
-        var templeftprof1 = Console.ReadLine();
-        var leftprof1 = CreateProfessionalByCodeName(templeftprof1);
-        if (leftprof1 != null)
+
+
+
+        var left = new Professional?[5];
+
+		var right = new Professional?[5];
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Enter left team professional at {i}");
+    Console.Write("CodeName:");
+    var tempLeftProf = Console.ReadLine();
+    var leftProf = CreateProfessionalByCodeName(tempLeftProf);
+    if (leftProf != null)
+    {
+        Console.Write("Attack:");
+        int attackValue;
+        while (!int.TryParse(Console.ReadLine(), out attackValue) || attackValue < 1 || attackValue > 50)
         {
+            Console.WriteLine("Invalid input. Please enter again.");
             Console.Write("Attack:");
-            int attackValue;
-            while (!int.TryParse(Console.ReadLine(), out attackValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Attack:");
-            }
-            leftprof1.Attack = attackValue;
+        }
+        leftProf.Attack = attackValue;
 
+        Console.Write("Health:");
+        int healthValue;
+        while (!int.TryParse(Console.ReadLine(), out healthValue) || healthValue < 1 || healthValue > 50)
+        {
+            Console.WriteLine("Invalid input. Please enter again.");
             Console.Write("Health:");
-            int healthValue;
-            while (!int.TryParse(Console.ReadLine(), out healthValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Health:");
-            }
-            leftprof1.Health = healthValue;
         }
-        else
+        leftProf.Health = healthValue;
+    }
+    else
+    {
+        Console.WriteLine($"Empty professional at index {i}");
+    }
+    left[i - 1] = leftProf;
+}
+
+
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Enter right team professional at {i}");
+    Console.Write("CodeName:");
+    var tempRightProf = Console.ReadLine();
+    var rightProf = CreateProfessionalByCodeName(tempRightProf);
+    if (rightProf != null)
+    {
+        Console.Write("Attack:");
+        int attackValue;
+        while (!int.TryParse(Console.ReadLine(), out attackValue) || attackValue < 1 || attackValue > 50)
         {
-            Console.WriteLine($"Empty professional at index 1");
-        }
-
-
-
-        Console.WriteLine("Enter left team professional at 2");
-        Console.Write("CodeName:");
-        var templeftprof2 = Console.ReadLine();
-        var leftprof2 = CreateProfessionalByCodeName(templeftprof2);
-        if (leftprof2 != null)
-        {
+            Console.WriteLine("Invalid input. Please enter again.");
             Console.Write("Attack:");
-            int attackValue;
-            while (!int.TryParse(Console.ReadLine(), out attackValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Attack:");
-            }
-            leftprof2.Attack = attackValue;
+        }
+        rightProf.Attack = attackValue;
 
-            Console.Write("Health:");
-            int healthValue;
-            while (!int.TryParse(Console.ReadLine(), out healthValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Health:");
-            }
-            leftprof2.Health = healthValue;
-        }
-        else
+        Console.Write("Health:");
+        int healthValue;
+        while (!int.TryParse(Console.ReadLine(), out healthValue) || healthValue < 1 || healthValue > 50)
         {
-            Console.WriteLine($"Empty professional at index 2");
+            Console.WriteLine("Invalid input. Please enter again.");
+            Console.Write("Health:");
         }
+        rightProf.Health = healthValue;
+    }
+    else
+    {
+        Console.WriteLine($"Empty professional at index {i}");
+    }
+    right[i - 1] = rightProf;
+}
         
 
-        Console.WriteLine("Enter left team professional at 3");
-        Console.Write("CodeName:");
-        var templeftprof3 = Console.ReadLine();
-        var leftprof3 = CreateProfessionalByCodeName(templeftprof3);
-        if (leftprof3 != null)
-        {
-            Console.Write("Attack:");
-            int attackValue;
-            while (!int.TryParse(Console.ReadLine(), out attackValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Attack:");
-            }
-            leftprof3.Attack = attackValue;
-
-            Console.Write("Health:");
-            int healthValue;
-            while (!int.TryParse(Console.ReadLine(), out healthValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Health:");
-            }
-            leftprof3.Health = healthValue;
-        }
-        else
-        {
-            Console.WriteLine($"Empty professional at index 3");
-        }
-
-        Console.WriteLine("Enter left team professional at 4");
-        Console.Write("CodeName:");
-        var templeftprof4 = Console.ReadLine();
-        var leftprof4 = CreateProfessionalByCodeName(templeftprof4);
-        if (leftprof4 != null)
-        {
-            Console.Write("Attack:");
-            int attackValue;
-            while (!int.TryParse(Console.ReadLine(), out attackValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Attack:");
-            }
-            leftprof4.Attack = attackValue;
-
-            Console.Write("Health:");
-            int healthValue;
-            while (!int.TryParse(Console.ReadLine(), out healthValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Health:");
-            }
-            leftprof4.Health = healthValue;
-        }
-        else
-        {
-            Console.WriteLine($"Empty professional at index 4");
-        }
-
-        Console.WriteLine("Enter left team professional at 5");
-        Console.Write("CodeName:");
-        var templeftprof5 = Console.ReadLine();
-        var leftprof5 = CreateProfessionalByCodeName(templeftprof5);
-        if (leftprof5 != null)
-        {
-            Console.Write("Attack:");
-            int attackValue;
-            while (!int.TryParse(Console.ReadLine(), out attackValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Attack:");
-            }
-            leftprof5.Attack = attackValue;
-
-            Console.Write("Health:");
-            int healthValue;
-            while (!int.TryParse(Console.ReadLine(), out healthValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Health:");
-            }
-            leftprof5.Health = healthValue;
-        }
-        else
-        {
-            Console.WriteLine($"Empty professional at index 5");
-        }
-
-        //Create right team
-
-        Console.WriteLine("Enter right team professional at 1");
-        Console.Write("CodeName:");
-        var temprightprof1 = Console.ReadLine();
-        var rightprof1 = CreateProfessionalByCodeName(temprightprof1);
-        if (rightprof1 != null)
-        {
-            Console.Write("Attack:");
-            int attackValue;
-            while (!int.TryParse(Console.ReadLine(), out attackValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Attack:");
-            }
-            rightprof1.Attack = attackValue;
-
-            Console.Write("Health:");
-            int healthValue;
-            while (!int.TryParse(Console.ReadLine(), out healthValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Health:");
-            }
-            rightprof1.Health = healthValue;
-        }
-        else
-        {
-            Console.WriteLine($"Empty professional at index 1");
-        }
-        
-
-        Console.WriteLine("Enter right team professional at 2");
-        Console.Write("CodeName:");
-        var temprightprof2 = Console.ReadLine();
-        var rightprof2 = CreateProfessionalByCodeName(temprightprof2);
-        if (rightprof2 != null)
-        {
-            Console.Write("Attack:");
-            int attackValue;
-            while (!int.TryParse(Console.ReadLine(), out attackValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Attack:");
-            }
-            rightprof2.Attack = attackValue;
-
-            Console.Write("Health:");
-            int healthValue;
-            while (!int.TryParse(Console.ReadLine(), out healthValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Health:");
-            }
-            rightprof2.Health = healthValue;
-        }
-        else
-        {
-            Console.WriteLine($"Empty professional at index 2");
-        }
-
-
-        Console.WriteLine("Enter right team professional at 3");
-        Console.Write("CodeName:");
-        var temprightprof3 = Console.ReadLine();
-        var rightprof3 = CreateProfessionalByCodeName(temprightprof3);
-        if (rightprof3 != null)
-        {
-            Console.Write("Attack:");
-            int attackValue;
-            while (!int.TryParse(Console.ReadLine(), out attackValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Attack:");
-            }
-            rightprof3.Attack = attackValue;
-
-            Console.Write("Health:");
-            int healthValue;
-            while (!int.TryParse(Console.ReadLine(), out healthValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Health:");
-            }
-            rightprof3.Health = healthValue;
-        }
-        else
-        {
-            Console.WriteLine($"Empty professional at index 3");
-        }
-
-
-        Console.WriteLine("Enter right team professional at 4");
-        Console.Write("CodeName:"); 
-        var temprightprof4 = Console.ReadLine();
-        var rightprof4 = CreateProfessionalByCodeName(temprightprof4);
-        
-        if (rightprof4 != null)
-        {
-            Console.Write("Attack:");
-            int attackValue;
-            while (!int.TryParse(Console.ReadLine(), out attackValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Attack:");
-            }
-            rightprof4.Attack = attackValue;
-
-            Console.Write("Health:");
-            int healthValue;
-            while (!int.TryParse(Console.ReadLine(), out healthValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Health:");
-            }
-            rightprof4.Health = healthValue;
-        }
-        else
-        {
-            Console.WriteLine($"Empty professional at index 4");
-        }
-            
-
-
-
-        Console.WriteLine("Enter right team professional at 5");
-        Console.Write("CodeName:");
-        var temprightprof5 = Console.ReadLine();
-        var rightprof5 = CreateProfessionalByCodeName(temprightprof5);
-        if (rightprof5 != null)
-        {
-            Console.Write("Attack:");
-            int attackValue;
-            while (!int.TryParse(Console.ReadLine(), out attackValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Attack:");
-            }
-            rightprof5.Attack = attackValue;
-
-            Console.Write("Health:");
-            int healthValue;
-            while (!int.TryParse(Console.ReadLine(), out healthValue))
-            {
-                Console.WriteLine("Invalid input. Please enter an integer value.");
-                Console.Write("Health:");
-            }
-            rightprof5.Health = healthValue;
-        }
-        else
-        {
-            Console.WriteLine($"Empty professional at index 5");
-        }
-
-        
-
-		var left = new Professional?[]
-		{
-            
-            leftprof1,
-			leftprof2,
-			leftprof3,
-			leftprof4,
-			leftprof5
-		};
-
-		var right = new Professional?[]
-		{
-			rightprof1,
-			rightprof2,
-			rightprof3,
-			rightprof4,
-			rightprof5
-		};
 
 		var game = new Game(new Team(left, Side.Left), new Team(right, Side.Right));
 
